39be8f243f25d587b51079838d435b9631ab212d
Pavithraramesh19 <pavithraramesh1902@gmail.com>
2024-02-26 18:26:10 +0530

    first commit

---
+++ phaseendPizzademo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace phaseendPizzademo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ phaseendPizzademo/Models/OrderInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace phaseendPizzademo.Models
+{
+    public class OrderInfo : Pizza
+    {
+        public int OrderId { get; set; }
+
+        public int TotalPrice { get; set; }
+    }
+}
